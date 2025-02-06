8fa356728fe4bcd156ac94f9bcee557e81ca68ed
IavoAmbinintsoa <167605546+IavoAmbinintsoa@users.noreply.github.com>
2025-02-06 10:11:51 +0000

    Update arithmetic.h

--- Math/arithmetic.h
+++ Math/arithmetic.h
@@ -1,9 +1,20 @@
 #include "Complex.h"
 /*
-    This headers is to calculate some basic operation with different type
-    or same type if we not know what type we use
- */
+    This header is used to perform basic operations with different types  
+    or the same type when the exact type is unknown.
+*/
+
 isa::Complex addition(isa::Complex a,isa::Rationnel b);
 isa::Complex addition(isa::Complex a,int i);
 isa::Complex addition(isa::Rationnel a,isa::Rationnel b);
 isa::Complex addition(isa::Complex a,isa::Complex b);
+
+isa::Complex soustraction(isa::Complex a,isa::Rationnel b);
+isa::Complex soustraction(isa::Complex a,int i);
+isa::Complex soustraction(isa::Rationnel a,isa::Rationnel b);
+isa::Complex soustraction(isa::Complex a,isa::Complex b);
+
+isa::Complex multiplication(isa::Complex a,isa::Rationnel b);
+isa::Complex multiplication(isa::Complex a,int i);
+isa::Complex multiplication(isa::Rationnel a,isa::Rationnel b);
+isa::Complex multiplication(isa::Complex a,isa::Complex b);
