1535ccfc7e982545324debcf5badfbbdf35fb8b7
IavoAmbinintsoa <167605546+IavoAmbinintsoa@users.noreply.github.com>
2025-02-06 10:16:09 +0000

    Update aritmetic.cpp

--- Math/aritmetic.cpp
+++ Math/aritmetic.cpp
@@ -30,3 +30,50 @@ Complex addition(Complex a,Complex b)
 {
     return a.addition(b);
 }
+Complex soustraction(Complex a,Rationnel b)
+{
+    Complex result = Complex();
+    result.setReal(b.soustraction(a.getReal()));
+    result.setImaginary(a.getImaginary());
+    return result;
+}
+Complex soustraction(Complex a,int i)
+{
+    Rationnel b = Rationnel(i,1);
+    return soustraction(a,b);
+}
+Complex soustraction(Rationnel a,Rationnel b)
+{
+    Complex temp = Complex();
+    temp.setReal(a.soustraction(b));
+    a.setNumerateur(0);
+    temp.setImaginary(a);
+    return temp;
+}
+Complex soustraction(Complex a,Complex b)
+{
+    return a.soustraction(b);
+}
+Complex multiplication(Complex a,Rationnel b)
+{
+    Complex result = Complex();
+    result.setReal(b.multiplication(a.getReal()));
+    return result.multiplication(a);
+}
+Complex multiplication(Complex a,int i)
+{
+    Rationnel b = Rationnel(i,1);
+    return multiplication(a,b);
+}
+Complex multiplication(Rationnel a,Rationnel b)
+{
+    Complex temp = Complex();
+    temp.setReal(a.multiplication(b));
+    a.setNumerateur(0);
+    temp.setImaginary(a);
+    return temp;
+}
+Complex multiplication(Complex a,Complex b)
+{
+    return a.multiplication(b);
+}
